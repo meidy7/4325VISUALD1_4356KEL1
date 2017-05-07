c1d4b75719ffbe4ebaa202c8a2cdbee6aa396257
Binti Musyawirotul Islamiyah <bintijantera@gmail.com>
2017-05-07 11:51:27 +0800

    penggajihan

---
+++ src/penggajian/panelGambar.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penggajian;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Islamiyah
+ */
+public class panelGambar extends JPanel {
+    private Image image; // membuat variable image
+     public panelGambar() {
+        image = new ImageIcon(getClass().getResource("/gambar/s5_background1.jpg")).getImage();
+        //memanggil sumber daya gambar
+    }
+     @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+     
+        Graphics gd = (Graphics2D) g.create();
+     
+        gd.drawImage(image, 0,0,getWidth(),getHeight(), this);
+        // menggambar image
+        gd.dispose();
+    }
+    
+}
